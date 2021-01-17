e17a259e8ed2e9894d078fba407b00014527364e
Hatice <haticekaraty@gmail.com>
2021-01-16 21:39:44 -0500

    commit tree 7829401e3c94c85be186f7a25cbd173c405632a5

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        return array[0]
+    elsif array.length == 2
+        return array.join(" and ")
+    else
+        lastElement = array.pop
+        innerString = array.join(', ')
+        finalString = innerString + ", and " + lastElement
+        return finalString
+    end
 end
